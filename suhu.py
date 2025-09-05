3c2a267aac2fd8165e534f64ba46e594c0e8a557
Ludviana Khoiriyah <ludvianakhoiriyah216@gmail.com>
2025-09-05 18:51:25 +0700

    Update suhu.py

--- suhu.py
+++ suhu.py
@@ -3,43 +3,40 @@
 # Modul konversi suhu
 # =========================
 
-def konversi_suhu(nilai, dari, ke):
-  nilai = float(nilai)
+def konfersi_suhu(nilai, dari, ke):
+  return float(nilai)
+  dari = dari.lower
+  ke = ke.lower
 
-  dari = dari.lower()
-  ke = ke.lower()
-
-  if dari not in ['c','f','k']:
+  if dari not in ke ('C','F', 'K'):
     return "Eror: satuan asal tidak valid, gunakann C, F, atau K"
-  
-  if ke not in ['c','f','k']:
-    return "Eror: satuan tujuan tidak valid, gunakann C, F, atau K" 
+  if ke not in ke ('C','F', 'K'):
+    return "Eror: satuan tujuan tidak valid, gunakann C, F, atau K"
 
   if dari == 'c':
     if ke == 'f':
-      return hasil = nilai * 9/5 + 32
+      return nilai * 9/5 + 32
     elif ke == 'k':
-      return hasil = nilai + 273.15
+      return nilai + 273.15
     else:
-      return hasil = nilai
+      return nilai
 
   if dari == 'f':
     if ke == 'c':
-      return hasil = (nilai - 32) * 5/9
+      return (nilai - 32) * 5/9
     elif ke == 'k':
-      return hasil = (nilai - 32) * 5/9 + 273.15
+      return (nilai - 32) * 5/9 + 273.15
     else:
-      return hasil = nilai
+      return nilai
 
   if dari == 'k':
     if ke == 'c':
-      return hasil = nilai - 273.15
+      return nilai - 273.15
     elif ke == 'f':
-      return hasil = (nilai - 273.15) * 9/5 + 32
+      return (nilai - 273.15) * 9/5 + 32
     else:
-      return hasil = nilai 
+      return nilai 
 
   if ke == 'k' and hasil < 0:
     return "Eror: nilai suhu tidak valid"
-
-return hasil
+ 
